3632ae40a50535bacaa921ea7b5a599f9addc7ee
najim2004 <najim10112004@gmail.com>
2024-06-01 23:59:43 +0600

    created users post api

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // ----------------import--------------------
-
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
@@ -20,6 +20,52 @@ app.use(
   //   }
 );
 app.use(express.json());
+
+// ---------------mongodb--------------
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.d0cidbu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    // All collections
+    const database = client.db("MultiCreatify_DB");
+    const usersCollection = await database.collection("Users");
+
+    // post a user info
+    app.post("/users", async (req, res) => {
+      const newUser = req.body;
+      const query = { email: newUser.email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({
+          message: "User already exists",
+        });
+      }
+      const result = await usersCollection.insertOne(newUser);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("the server is running");
