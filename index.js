1da10070bf3c5a593242ba80596821a7f22d6462
najim2004 <najim10112004@gmail.com>
2024-06-06 00:51:36 +0600

    added jwt security system

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -10,16 +12,40 @@ const port = process.env.PORT || 5000;
 
 // ---------------------middleware--------------------
 app.use(
-  cors()
-  //     {
-  //     origin: [
-  //       "http://localhost:5173",
-  //       "http://localhost:5174",
-  //     ],
-  //     credentials: true,
-  //   }
+  cors({
+    origin: ["http://localhost:5173", "http://localhost:5174"],
+    credentials: true,
+  })
 );
 app.use(express.json());
+app.use(cookieParser());
+
+const cookieOptions = {
+  httpOnly: true,
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+  secure: process.env.NODE_ENV === "production" ? true : false,
+};
+
+// logger
+const logger = async (req, res, next) => {
+  console.log("called:", req.host, req.originalUrl);
+  next();
+};
+
+// verify token
+const verifyToken = (req, res, next) => {
+  const token = req.cookies?.token;
+  if (!token) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "unauthorized access" });
+    }
+    req.user = decoded;
+    next();
+  });
+};
 
 // ---------------mongodb--------------
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.d0cidbu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -43,8 +69,58 @@ async function run() {
     const worksheetCollection = await database.collection("WorkSheet");
     const paymentsCollection = await database.collection("Payments");
 
+    // verify Admin
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.user.email;
+      const query = { email: email };
+      const response = await usersCollection.findOne(query);
+      if (response.role === "Admin") {
+        next();
+      } else {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+    };
+    // verify HR
+    const verifyHR = async (req, res, next) => {
+      const email = req.user.email;
+      const query = { email: email };
+      const response = await usersCollection.findOne(query);
+      if (response.role === "HR") {
+        next();
+      } else {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+    };
+    // verify employee
+    const verifyEmployee = async (req, res, next) => {
+      const email = req.user.email;
+      const query = { email: email };
+      const response = await usersCollection.findOne(query);
+      if (response.role === "Employee") {
+        next();
+      } else {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+    };
+    // verify employee & HR
+
+    // jwt related api
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
+        expiresIn: "1h",
+      });
+      res.cookie("token", token, cookieOptions).send({ success: true });
+    });
+
+    // remove cookies api
+    app.get("/logout", async (req, res) => {
+      res
+        .clearCookie("token", { ...cookieOptions, maxAge: 0 })
+        .send({ success: true });
+    });
     // post a user info
-    app.post("/users", async (req, res) => {
+    app.post("/users", logger, async (req, res) => {
       const newUser = req.body;
       const query = { email: newUser.email };
       const existingUser = await usersCollection.findOne(query);
@@ -56,76 +132,75 @@ async function run() {
       const result = await usersCollection.insertOne(newUser);
       res.send(result);
     });
-    // get all employee by get method
-    app.get("/users", async (req, res) => {
+    // get all employee by get method (only HR)
+    app.get("/users", logger, verifyToken, verifyHR, async (req, res) => {
       const role = req.query.role;
       const query = { role: role };
       const result = await usersCollection.find(query).toArray();
       res.send(result);
     });
 
-    // get all employee & HR by get method
-    app.get("/users/admin", async (req, res) => {
-      const query = {
-        rol: { $ne: "Admin" },
-        verified: { $ne: false },
-      };
-      const result = await usersCollection.find(query).toArray();
-      res.send(result);
-    });
+    // get all employee & HR by get method (only admin)
+    app.get(
+      "/users/admin",
+      logger,
+      verifyToken,
+      verifyAdmin,
+      async (req, res) => {
+        const query = {
+          role: { $ne: "Admin" },
+          verified: { $ne: false },
+        };
+        const result = await usersCollection.find(query).toArray();
+        res.send(result);
+      }
+    );
 
-    // update the user info by patch method
-    app.patch("/users/:id", async (req, res) => {
-      const id = req.params.id;
-      const newData = req.body;
-      const updateDoc = { $set: { ...newData } };
-      const query = { _id: new ObjectId(id) };
-      const result = await usersCollection.updateOne(query, updateDoc);
-      res.send(result);
-    });
-    // get user by id by get method
-    app.get("/users/:id", async (req, res) => {
+    // update the user info by patch method (only admin)
+    app.patch(
+      "/users/:id",
+      logger,
+      verifyToken,
+      verifyAdmin,
+      async (req, res) => {
+        const id = req.params.id;
+        const newData = req.body;
+        const updateDoc = { $set: { ...newData } };
+        const query = { _id: new ObjectId(id) };
+        const result = await usersCollection.updateOne(query, updateDoc);
+        res.send(result);
+      }
+    );
+    // get user by id by get method (only HR)
+    app.get("/users/:id", logger, verifyToken, verifyHR, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await usersCollection.findOne(query);
       res.send(result);
     });
 
-    // get user by email by get method
-    app.get("/users/email/:email", async (req, res) => {
+    // get user by email by get method (everyone)
+    app.get("/users/role/:email", async (req, res) => {
       const email = req.params.email;
       const query = { email: email };
       const result = await usersCollection.findOne(query);
-      res.send(result);
+      res.send({ role: result.role });
     });
 
-    // post new work by post method
-    app.post("/work-sheet", async (req, res) => {
+    // post new work by post method (Employee only)
+    app.post("/work-sheet", logger, verifyToken, async (req, res) => {
       const newWork = req.body;
       const result = await worksheetCollection.insertOne(newWork);
       res.send(result);
     });
-    // get all worksheets
-    app.get("/work-sheet", async (req, res) => {
-      const { employee, month } = req.query;
-      let query = {};
-
-      if (employee) {
-        query.name = employee;
-      }
-      if (month) {
-        const monthInt = parseInt(month, 10);
-        query.date = {
-          $gte: new Date(2024, monthInt, 1), // Change the year as needed
-          $lt: new Date(2024, monthInt + 1, 1), // Change the year as needed
-        };
-      }
-      const result = await worksheetCollection.find(query).toArray();
+    // get all worksheets (HR only)
+    app.get("/work-sheet", logger, verifyToken, verifyHR, async (req, res) => {
+      const result = await worksheetCollection.find().toArray();
       res.send(result);
     });
 
-    // get specific user works data
-    app.get("/work-sheet/:email", async (req, res) => {
+    // get specific user works data (Employee Only)
+    app.get("/work-sheet/:email", logger, verifyToken, async (req, res) => {
       const email = req.params.email;
       const query = { email: email };
       const result = await worksheetCollection
@@ -135,8 +210,8 @@ async function run() {
       res.send(result);
     });
 
-    // get single user payment information
-    app.get("/salary-history/:email", async (req, res) => {
+    // get single user payment information (Employee only)
+    app.get("/salary-history/:email", logger, verifyToken, async (req, res) => {
       const email = req.params.email;
       const query = { email: email };
       const result = await paymentsCollection
